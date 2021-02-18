c3e8bf4448a127c3455f5ad83b4d3c97110b9e79
Gozel Cholukova <checkboxtm@gmail.com>
2021-02-19 02:21:24 +0500

    Update react.js

--- react.js
+++ react.js
@@ -1,24 +1,61 @@
-class App extends React.Component {
-  state = {
-    keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-  }
-  
-  render() {
-    const { keys } = this.state;
-    return (
-      <div id="display" className="display">
-        {keys.map((key, idx) => (
-          <Box text={key} key={idx} />
-          ))}
-      </div>
-    );
-  }
-}
+const sounds = [
+  {
+    key: 'Q',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    key: 'W',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    key: 'E',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    key: 'A',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    key: 'S',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    key: 'D',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    key: 'Z',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    key: 'X',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    key: 'C',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  },
+]
 
-const Box = () => (
-  <div className="box">
-    {props.text}
+const App = () => (
+  <div id="display" className="display">
+    {sounds.map((sound, idx) => (
+      <Box text={sound.key} key={idx} audio={sound.mp3} />
+    ))}
   </div>
 );
+
+class Box extends React.Component {
+  render() {
+    const { text, audio } = this.props;
+    
+    return (
+      <div className="box">
+        {text}
+        <audio src={audio} className="clip" id={text} />
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('drum-machine'));
